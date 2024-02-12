8e98bff96210844503f67ccbea46e02b075d3ad5
Andrew <andrew@hypar.io>
2024-02-12 15:40:10 -0500

    initial commit

---
+++ dummy.test.js
@@ -0,0 +1,10 @@
+test('should return hello world', () => {
+  // Arrange
+  const expected = 'hello world';
+
+  // Act
+  const result = 'hello world';
+
+  // Assert
+  expect(result).toBe(expected);
+});
